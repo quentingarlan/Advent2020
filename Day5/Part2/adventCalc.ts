a80fb330ac42d3ed73205a43a158aef1ab7d71d8
Quentin.GARLAN <Quentin.GARLAN@solina-group.fr>
2021-11-29 16:03:28 +0100

    tried part 2 day 5

--- Day5/Part2/adventCalc.ts
+++ Day5/Part2/adventCalc.ts
@@ -1,43 +1,54 @@
+
+let maxRows = 127
+let maxColumns = 7
+
 export function BinaryBoardingList(advList: string[]): number {
   let highestSeatId = 0;
-  advList.forEach(elt =>{
-    let nb = BinaryBoarding(elt);
-    if (nb > highestSeatId){
-      highestSeatId = nb;
+  
+  let seats:Map<{}, boolean> = new Map<{}, boolean>();
+  for (let i = 0; i < maxRows; i++) {
+    for (let j = 0; j < maxColumns; j++) {
+      seats.set({i,  j}, false);
     }
-  });
+  }
+  console.log("seats count", seats.keys.length)
+  //Enter the taken seats
+  advList.forEach(elt => {
+    let { maxRow, maxColumn } = GetRowCol(elt);
+    // console.log("{ maxRow, maxColumn } ", { maxRow, maxColumn } )
+      seats.set({maxRow, maxColumn}, true);
+  })
+  let mySeat = Array.from(seats.values()).filter((item) => item === false);
+  console.log("mySeat", mySeat.values())
+
   return highestSeatId;
 }
 
 export function BinaryBoarding(advList: string): number {
-  let {maxRow, maxColumn} = GetRowCol(advList);
-  console.log("maxRow", maxRow )
-  console.log("maxColumn", maxColumn  )
+  let { maxRow, maxColumn } = GetRowCol(advList);
   return maxRow * 8 + maxColumn;
 }
 
-function GetRowCol(advList: string): {maxRow:number, maxColumn:number} {
-  let maxRows    = 127
-	let maxColumns = 7
+function GetRowCol(advList: string): { maxRow: number, maxColumn: number } {
   let minRow = 0;
   let maxRow = maxRows;
   let minColumn = 0;
   let maxColumn = maxColumns;
-  for (let i = 0; i< advList.length; i++){
+  for (let i = 0; i < advList.length; i++) {
     let char = advList.charAt(i);
 
-    if (char === "F"){
-      maxRow = Math.trunc((minRow + maxRow) /2);
+    if (char === "F") {
+      maxRow = Math.trunc((minRow + maxRow) / 2);
     }
-    if (char === "B"){
-      minRow =  Math.trunc((minRow + maxRow) /2);
+    if (char === "B") {
+      minRow = Math.trunc((minRow + maxRow) / 2);
     }
-    if (char === "R"){
-      minColumn =  Math.trunc((minColumn + maxColumn) /2);
+    if (char === "R") {
+      minColumn = Math.trunc((minColumn + maxColumn) / 2);
     }
-    if (char === "L"){
-      maxColumn =  Math.trunc((minColumn + maxColumn) /2);
-    } 
+    if (char === "L") {
+      maxColumn = Math.trunc((minColumn + maxColumn) / 2);
+    }
   }
-  return {maxRow, maxColumn}
+  return { maxRow, maxColumn }
 }
