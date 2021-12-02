b1c9c5d92af5bfc1c9afc44e3acc0d29003b32aa
Quentin.GARLAN <Quentin.GARLAN@solina-group.fr>
2021-12-02 08:56:47 +0100

    improve day 7

--- Day7/Part1/adventCalc.ts
+++ Day7/Part1/adventCalc.ts
@@ -1,66 +1,110 @@
+const searchedColorAdjective: string = 'shiny';
+const searchedColor: string = 'gold';
+
 export function HandyHaversacks(advList: string): number {
   const listOfRules = advList.split('\n');
 
-  let count = 0;
   let containerBags: container[] = [];
   //Check if bags can contain our bag
   let bags: container[] = [];
+  let uniqueBags = new Set<string>();
+
+  // check which bags directly contain our searched bag
   listOfRules.forEach(elt => {
     bags = GetContainedBags(elt);
-    const container: container = FillContainers(elt, 'shiny', 'gold', bags)
+    const directContainer: container = FillContainers(elt, bags)
 
-    if (container) {
-      if (container.colorAdjective) {
-        containerBags.push(container);
+    if (directContainer) {
+      if (directContainer.colorAdjective) {
+        containerBags.push(directContainer);
       }
     }
   })
 
+
+  // check which bags contain bag that can contain our searched bag
+  listOfRules.forEach(elt => {
+    bags = GetContainedBags(elt);
+    const secondLevelContainer: container = FillSecondlevelContainers(elt, bags, containerBags)
+
+    if (secondLevelContainer) {
+      if (secondLevelContainer.colorAdjective) {
+        containerBags.push(secondLevelContainer);
+      }
+    }
+  })
+  console.log("containerBags", containerBags)
+
+
   // check rules
   listOfRules.forEach(elt => {
     bags = GetContainedBags(elt);
-    console.log(bags)
-    const ruleOk = CheckRules('shiny', 'gold', containerBags, bags)
-    if (ruleOk) {
-      count++;
+
+    const returnedBag = CheckRules(elt, containerBags, bags)
+    if (returnedBag !== null) {
+      console.log("bag", returnedBag)
+      uniqueBags.add(returnedBag.color + returnedBag.colorAdjective);
     }
   }
   )
-  return count;
+  console.log("uniqueBags", uniqueBags)
+  console.log(" uniqueBags.keys.length", uniqueBags.size)
+  return uniqueBags.size;
 }
 
-function FillContainers(rule: string, adjective: string, color: string, bags: container[]): container {
+function CheckRules(firstBag: string, containerBags: container[], bags: container[]): container {
+  let returnedBag = null;
+  const rules = firstBag.split(' ');
+
+  const firstColorAdjective = rules[0];
+  const firstColor = rules[1];
+
+  bags.forEach(bag => {
+
+    const bagIsContainer = containerBags.some(elt => elt.colorAdjective === firstColorAdjective && elt.color === firstColor)
+    const bagContainsContainer = containerBags.some(elt => elt.colorAdjective === bag.colorAdjective && elt.color === bag.color)
+    if (bagContainsContainer || bagIsContainer) {
+      let returnedContainer = new container();
+      returnedContainer.color = firstColor;
+      returnedContainer.colorAdjective = firstColorAdjective;
+      returnedBag =  returnedContainer;
+    }
+  })
+  return returnedBag;
+}
+
+function FillContainers(rule: string, bags: container[]): container {
   const rules = rule.split(' ');
 
   const firstColorAdjective = rules[0];
   const firstColor = rules[1];
+
   let returnedBag: container = null;
 
   bags.forEach(bag => {
-    if (bag.color === color && bag.colorAdjective === adjective) {
+    if (bag.color === searchedColor && bag.colorAdjective === searchedColorAdjective) {
       returnedBag = { colorAdjective: firstColorAdjective, color: firstColor }
     }
   })
   return returnedBag;
 }
 
-function CheckRules(adjective: string, color: string, containerBags: container[], bags: container[]): boolean {
-  let bagCanContain= false;
+function FillSecondlevelContainers(rule: string, bags: container[], containerBags: container[]): container {
+  const rules = rule.split(' ');
+
+  const firstColorAdjective = rules[0];
+  const firstColor = rules[1];
+
+  let returnedBag: container = null;
 
   bags.forEach(bag => {
-    const bagIsContainer = containerBags.some(elt => elt.colorAdjective === bag.colorAdjective && elt.color === bag.color)
-    if (bag.color === color && bag.colorAdjective === adjective) {
-      console.log("bag.color", bag.color)
-      console.log("bag.colorAdjective", bag.colorAdjective)
-      bagCanContain = true;
-    }else if (bagIsContainer){
-      console.log("bagIsContainer", bagIsContainer)
-      bagCanContain = true;
-    }else{
-      bagCanContain = false;
-    }
+    containerBags.forEach(elt => {
+      if (elt.color === bag.color && elt.colorAdjective === bag.colorAdjective) {
+        returnedBag = { colorAdjective: firstColorAdjective, color: firstColor }
+      }
+    })
   })
-  return bagCanContain;
+  return returnedBag;
 }
 
 function GetContainedBags(rules: string): container[] {
